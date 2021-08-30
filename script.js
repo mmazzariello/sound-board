b69d20e330ac30e18c5244fbc8525da8567d6a69
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-08-30 17:19:09 +0200

    ADD js code

---
+++ script.js
@@ -0,0 +1,10 @@
+const sounds = ["rain", "deep", "under", "rain-bis", "deep-bis", "under-bis"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  document.getElementById("buttons").appendChild(btn);
+});
